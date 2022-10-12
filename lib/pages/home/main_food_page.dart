2972468b729e32faf192ecb39376704598c13548
onukingsley <onu.nnamdi.2000@gmail.com>
2022-10-12 11:08:46 +0100

    shopping app Flutter

---
+++ lib/pages/home/main_food_page.dart
@@ -0,0 +1,63 @@
+import 'package:e_commerce/pages/home/food_page_body.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/small_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../../utils/colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+              child: Container(
+                  margin: EdgeInsets.only(top: 45, bottom: 10),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          BigText(
+                              text: "Bangaladash", color: AppColors.mainColor),
+                          Row(
+                            children: [
+                              SmallText(text: "Narsingdi"),
+                              Icon(Icons.arrow_drop_down_rounded)
+                            ],
+                          )
+                        ],
+                      ),
+                      Center(
+                        child: Container(
+                          width: 45,
+                          height: 45,
+                          child: Icon(Icons.search, color: Colors.white),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: AppColors.mainColor),
+                        ),
+                      ),
+                    ],
+                  ))),
+          Expanded(
+              child: SingleChildScrollView(
+            child: FoodPageBody(),
+          ))
+        ],
+      ),
+    );
+  }
+}
